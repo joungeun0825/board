d9af6780c0763e0b94267f1073532b91ceae9dc7
joungeun0825 <joungeun0825@gmail.com>
2025-02-11 23:05:57 +0900

    fix: 작성 일자 추가 및 오류 수정

--- src/main/java/board/ift/post/Post.java
+++ src/main/java/board/ift/post/Post.java
@@ -1,17 +1,22 @@
 package board.ift.post;
 
-import board.ift.base.BaseTimeEntity;
 import board.ift.user.User;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Builder
-public class Post extends BaseTimeEntity {
+public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -24,6 +29,13 @@ public class Post extends BaseTimeEntity {
 
     @Column(nullable = false)
     private boolean status;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDate createdDate;
+
+    @LastModifiedDate
+    private LocalDate lastModifiedDate;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
